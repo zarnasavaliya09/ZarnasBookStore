c79b2114852f106a528bc7f5d57fd5a7d31a8299
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-04-12 21:11:23 -0400

    added Upsert view in CategoryController

--- ZarnasBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ ZarnasBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -16,33 +16,74 @@ namespace ZarnasBookStore.Areas.Admin.Controllers
         {
             _unitOfWork = unitOfWork;
         }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult Upsert(int? id)
+
+        public IActionResult Upsert(int? id) //Action method for Upsert
         {
-            Category category = new Category();
+            Category category = new Category(); 
             if (id == null)
             {
+                //This is for Create
                 return View(category);
             }
+            //This is for the Edit
             category = _unitOfWork.Category.Get(id.GetValueOrDefault());
             if (category == null)
             {
                 return NotFound();
             }
-
-            return View();
+            return View(category);
         }
+
+        //Use HTTP POST to define the post-action method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid) //Checks all the Validation in the model (e.g. Name Required) to increase security
+            {
+                if (category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index)); //To see all the Categories
+            }
+            return View(category);
+        }
+
+        //API calls here
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
+            //return NotFound();
             var allObj = _unitOfWork.Category.GetAll();
-            Console.WriteLine(allObj);
             return Json(new { data = allObj });
         }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
         #endregion
+
+
     }
 }
