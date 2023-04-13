e5bdec85be899bc529ab47d8c42d4b583d2111a1
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-04-13 16:31:20 -0400

    create model class

---
+++ ZarnasBooks.Models/CoverType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ZarnasBooks.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Cover Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
