ef0c82bfa527558d0b8408fdb4d99cede4af43d0
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-04-14 11:35:44 -0400

    create model class in .DataAccess

---
+++ ZarnasBooks.Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ZarnasBooks.Models
+{
+   public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+        [Required]
+        [Range(1,10000)]
+        public double ListPrice { get; set; }
+        public string ImageUrl { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        [Required]
+        public int CoverTypeId { get; set; }
+        [ForeignKey("CoverTypeId")]
+        public CoverType CoverType { get; set; }
+    }
+}
