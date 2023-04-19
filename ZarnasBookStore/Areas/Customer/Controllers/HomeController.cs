e1b001e1d8a36bac1ab4daff8c305196a387e49f
zarnasavaliya09 <113407633+zarnasavaliya09@users.noreply.github.com>
2023-04-19 16:05:47 -0400

    update HomeController in Customer

--- ZarnasBookStore/Areas/Customer/Controllers/HomeController.cs
+++ ZarnasBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ZarnasBooks.DataAccess.Repository.IRepository;
+using ZarnasBooks.Models;
 using ZarnasBookStore.Models;
 using ZarnasBookStore.Models.ViewModels;
 
@@ -14,15 +16,18 @@ namespace ZarnasBookStore.Area.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unifOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unifOfWork)
         {
             _logger = logger;
+            _unifOfWork = unifOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unifOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+            return View(productList);
         }
 
         public IActionResult Privacy()
